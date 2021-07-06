7102fdbe145ae7b6b02ba942a4955b9400ca5e5f
Герман Лепин <g.lepin@mail.ru>
2021-07-06 15:20:25 +0300

    debug commit

--- Polling/models.py
+++ Polling/models.py
@@ -2,27 +2,39 @@ from django.db import models
 
 
 class Poll(models.Model):
-    poll_name = models.CharField(max_length=200)
-    pub_date = models.DateTimeField()
-    end_date = models.DateTimeField()
-    poll_description = models.CharField(max_length=200)
+    poll_name = models.CharField(max_length=200, verbose_name='Название опроса')
+    pub_date = models.DateTimeField(verbose_name='Дата и время начала опроса')
+    end_date = models.DateTimeField(verbose_name='Дата и время окончания опроса')
+    poll_description = models.CharField(max_length=200, verbose_name='Описание опроса')
+
+    class Meta:
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
 
     def __str__(self):
         return self.poll_name
 
 
 class Question(models.Model):
-    poll = models.ForeignKey(Poll, related_name='questions', on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=200)
-    question_type = models.CharField(max_length=200)
+    poll = models.ForeignKey(Poll, related_name='questions', verbose_name='Название опроса', on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200, verbose_name='Вопрос')
+    question_type = models.CharField(max_length=200, verbose_name='Тип вопроса')
 
     def __str__(self):
         return self.question_text
 
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, related_name='choices', on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    question = models.ForeignKey(Question, related_name='choices', verbose_name='Вопрос', on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200, verbose_name='Вариант ответа')
+
+    class Meta:
+        verbose_name = 'Вариант ответа'
+        verbose_name_plural = 'Вариант ответов'
 
     def __str__(self):
         return self.choice_text
@@ -30,10 +42,14 @@ class Choice(models.Model):
 
 class Answer(models.Model):
     user_id = models.IntegerField()
-    poll = models.ForeignKey(Poll, related_name='poll', on_delete=models.CASCADE)
-    question = models.ForeignKey(Question, related_name='question', on_delete=models.CASCADE)
-    choice = models.ForeignKey(Choice, related_name='choice', on_delete=models.CASCADE, null=True)
-    choice_text = models.CharField(max_length=200, null=True)
+    poll = models.ForeignKey(Poll, related_name='poll', verbose_name='Опрос', on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, related_name='question', verbose_name='Вопрос', on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, related_name='choice', on_delete=models.CASCADE, verbose_name='Вариант ответа', null=True)
+    choice_text = models.CharField(max_length=200, verbose_name='Ответ', null=True)
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
 
     def __str__(self):
         return self.choice_text
